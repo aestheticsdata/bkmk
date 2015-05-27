ab7ca9e0f79c3a9fdc989e4997d3f223af0ffd32
scabel <aesthetics.data@gmail.com>
2015-05-27 18:19:36 +0200

    ui-bootstrap added (bower angular-bootstrap) / radio button added for view type

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // app.js
 
-angular.module('bkmk', ['bkmk.directives', 'bkmk.controllers', 'bkmk.services', 'ui.router'])
+angular.module('bkmk', ['ui.bootstrap', 'bkmk.directives', 'bkmk.controllers', 'bkmk.services', 'ui.router'])
     .config(function ($stateProvider, $urlRouterProvider) {
 
 
--- js/controllers/StatusBarController.js
+++ js/controllers/StatusBarController.js
@@ -5,7 +5,12 @@ angular.module('bkmk.controllers').controller('StatusBarController', StatusBarCo
 function StatusBarController($scope, $log, $state, $stateParams) {
 
     $scope.statusbar = {
-        status: $state.params.title
+        status      : $state.params.title,
+        searchbox   : false,
+        viewButtons : false,
+        viewType    : 'List'
     };
+
+    ($state.params.title === 'List All') && ($scope.statusbar.searchbox = true) && ($scope.statusbar.viewButtons = true);
 
 }
