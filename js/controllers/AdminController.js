78abecc98857a77d1c6d6ab73217e45835c44ac7
scabel <aesthetics.data@gmail.com>
2015-06-17 18:13:01 +0200

    font type added -> views updated accordingly

--- js/controllers/AdminController.js
+++ js/controllers/AdminController.js
@@ -7,4 +7,15 @@ function AdminController($scope, $log) {
     $log.debug('AdminController');
 
     $scope.isAdmin = true;
+
+    $scope.login = function () {
+
+        var user = document.querySelector('#userfield').value,
+            pass = document.querySelector('#passfield').value;
+
+
+        console.log('sign in');
+
+        $state.go();
+    }
 }
